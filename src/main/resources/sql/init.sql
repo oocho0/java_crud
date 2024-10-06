170764854d1ea06d6d687e7dce9c5414a8091260
oocho0 <rlawngml59@gmail.com>
2024-10-06 23:04:35 +0900

    DB에 사용한 DDL update

---
+++ src/main/resources/sql/init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE post (
+    postId INT(11) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    reg_date DATETIME NOT NULL,
+    mod_date DATETIME NOT NULL,
+    use_yn VARCHAR(1) NOT NULL,
+    PRIMARY KEY (postId)
+);
+
+CREATE TABLE reply (
+    replyId INT(11) NOT NULL AUTO_INCREMENT,
+    referId INT(11) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    reg_date DATETIME NOT NULL,
+    mod_date DATETIME NOT NULL,
+    use_yn VARCHAR(1) NOT NULL,
+    replyLevel INT(11) NOT NULL,
+    PRIMARY KEY (replyId)
+);
